4c4ca028208c1e7a987d6ea482380949f28bd0b8
MARCOSM-MARTINS <marcosm.caixa@gmail.com>
2020-09-10 14:01:28 -0300

    LoggingInterceptor

--- bytebank-master/lib/http/webclient.dart
+++ bytebank-master/lib/http/webclient.dart
@@ -1,6 +1,31 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print("Request");
+    print('Url: ${data.baseUrl}');
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print("Response");
+    print("Status code: ${data.statusCode}");
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get("http://192.168.1.14:8080/transactions");
-  print(response.body);
+  Client client = HttpClientWithInterceptor.build(interceptors: [
+    LoggingInterceptor(),
+  ]);
+  final Response response =
+      await client.get("http://192.168.1.15:8080/transactions");
 }
--- bytebank-master/lib/main.dart
+++ bytebank-master/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BytebankApp());
   findAll();
+  runApp(BytebankApp());
 }
 
 class BytebankApp extends StatelessWidget {
